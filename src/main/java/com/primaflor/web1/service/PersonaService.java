292d948f64721d49bcbc59af6cbdaed30419c4ae
rarnau <rarnau@arteco-consulting.com>
2024-05-22 11:36:26 +0200

    Otros servicios de interés

--- src/main/java/com/primaflor/web1/service/PersonaService.java
+++ src/main/java/com/primaflor/web1/service/PersonaService.java
@@ -6,10 +6,14 @@ import com.primaflor.web1.repository.PersonaRepository;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import jakarta.persistence.EntityManager;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Service
 public class PersonaService {
@@ -18,12 +22,29 @@ public class PersonaService {
 	private final EntityManager entityManager;
 
 	private final PersonaRepository personaRepository;
+	private final JdbcTemplate jdbcTemplate;
+	private final TransactionTemplate transactionTemplate;
+	private final ApplicationContext context;
 
 
-	public PersonaService(EntityManager entityManager, PersonaRepository personaRepository) {
+	public PersonaService(EntityManager entityManager, PersonaRepository personaRepository, JdbcTemplate jdbcTemplate,
+						  TransactionTemplate transactionTemplate, ApplicationContext context) {
 
 		this.entityManager = entityManager;
 		this.personaRepository = personaRepository;
+		this.jdbcTemplate = jdbcTemplate;
+		this.transactionTemplate = transactionTemplate;
+		this.context = context;
+
+		transactionTemplate.executeWithoutResult(txt -> {
+			// lógica que iría en una transacción
+			// find, delete, insert
+
+
+			// antes de la última llave, se haría commit si no hay error,
+			// rollback en caso contrario
+		});
+
 	}
 
 	@PostConstruct
@@ -32,6 +53,8 @@ public class PersonaService {
 
 		this.savePersona(new PersonaEntity(null, "Ramón", 43));
 		this.savePersona(new PersonaEntity(null, "Pepe", 47));
+
+		System.out.println(Arrays.stream(context.getBeanDefinitionNames()).toList());
 	}
 
 	@PreDestroy
@@ -46,6 +69,7 @@ public class PersonaService {
 
 
 	public Optional<PersonaEntity> getPersona(Integer idPersona) {
+
 		return this.personaRepository.findById(idPersona);
 	}
 
